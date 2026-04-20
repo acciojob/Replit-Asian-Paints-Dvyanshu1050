8006720b10730b6aa86c20871fc05da65dba0d40
DIVYANSHU KUMAR MAURYA <155415613+Dvyanshu1050@users.noreply.github.com>
2026-04-20 22:43:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+//your JS code here. If required
+document.getElementById("change_button").addEventListener("click", () => {
+
+  let blockId = document.getElementById("block_id").value;
+  let color = document.getElementById("colour_id").value;
+
+  let allBlocks = document.querySelectorAll(".grid-item");
+
+  // 🧠 step 1: sabko reset karo
+  allBlocks.forEach(item => {
+    item.style.backgroundColor = "transparent";
+  });
+
+  // 🧠 step 2: selected block ko color do
+  let selected = document.getElementById(blockId);
+  if (selected) {
+    selected.style.backgroundColor = color;
+  }
+});
+
+
+document.getElementById("Reset").addEventListener("click", () => {
+  let allBlocks = document.querySelectorAll(".grid-item");
+
+  allBlocks.forEach(item => {
+    item.style.backgroundColor = "transparent";
+  });
+});
